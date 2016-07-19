8605881e8a8205d002dce12725df1c44776d2579
owent <owt5008137@live.com>
2016-07-19 20:31:44 +0800

    reduce connection of atbus atproxy basic test passed

--- cmake_dev.bat
+++ cmake_dev.bat
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake .. -G "Visual Studio 14 2015 Win64" -DLIBUV_ROOT=C:\workspace\lib\network\libuv-win32 -DMSGPACK_ROOT=C:\workspace\lib\protocol\msgpack\prebuilt -DLIBCURL_ROOT=C:\workspace\lib\network\prebuilt\msvc -DPROJECT_ENABLE_SAMPLE=ON
+cmake .. -G "Visual Studio 14 2015 Win64" -DLIBUV_ROOT=C:\workspace\lib\network\libuv-win32 -DMSGPACK_ROOT=C:\workspace\lib\protocol\msgpack\prebuilt -DLIBCURL_ROOT=C:\workspace\lib\network\prebuilt\msvc -DPROJECT_ENABLE_SAMPLE=ON -DPROJECT_ENABLE_UNITTEST=ON
