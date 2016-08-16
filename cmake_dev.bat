712364b788a5e1eb5f0d17cd2942434f9a8e4c90
owt5008137 <owt5008137@live.com>
2016-08-16 17:49:43 +0800

    配置修订，临时加component的fake代码，用以帮助cmake判定语言

--- cmake_dev.bat
+++ cmake_dev.bat
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake .. -G "Visual Studio 14 2015 Win64" -DLIBUV_ROOT=C:\workspace\lib\network\libuv-win32 -DMSGPACK_ROOT=C:\workspace\lib\protocol\msgpack\prebuilt -DLIBCURL_ROOT=C:\workspace\lib\network\prebuilt\msvc -DPROJECT_ENABLE_SAMPLE=ON -DPROJECT_ENABLE_UNITTEST=ON
+cmake .. -G "Visual Studio 14 2015 Win64" -DLIBUV_ROOT=C:\workspace\lib\network\libuv-win32 -DMSGPACK_ROOT=C:\workspace\lib\protocol\msgpack\prebuilt -DLIBCURL_ROOT=C:\workspace\lib\network\prebuilt\msvc -DOPENSSL_ROOT_DIR=C:\workspace\lib\crypto\prebuilt\openssl-1.0.2h-vs2015 -DPROJECT_ENABLE_SAMPLE=ON -DPROJECT_ENABLE_UNITTEST=ON
