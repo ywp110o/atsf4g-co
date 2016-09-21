6585560db011ec1eb1572bdb641a498ff3237180
owent <owt5008137@live.com>
2016-09-21 12:22:00 +0800

    修订组件库名

--- atframework/atframework.macro.cmake
+++ atframework/atframework.macro.cmake
@@ -8,6 +8,6 @@ include("${ATFRAMEWORK_BASE_DIR}/libatbus/libatbus.cmake")
 include("${ATFRAMEWORK_BASE_DIR}/libatapp/libatapp.cmake")
 
 set(ATFRAMEWORK_SERVICE_COMPONENT_DIR "${ATFRAMEWORK_BASE_DIR}/service/component")
-set(ATFRAMEWORK_SERVICE_LINK_NAME libatservice_component)
+set(ATFRAMEWORK_SERVICE_LINK_NAME atservice_component)
 
 set(ATFRAMEWORK_SERVICE_GATEWAY_PROTOCOL_DIR "${ATFRAMEWORK_BASE_DIR}/service/atgateway/protocols")
