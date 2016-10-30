48bbc38657a0165141733e9ec295408e7745dd85
owt5008137 <owt5008137@live.com>
2016-10-30 17:12:00 +0800

    do not send command if proc not running

--- install/tools/script/helper/template/script/stop.sh
+++ install/tools/script/helper/template/script/stop.sh
@@ -2,6 +2,12 @@
     import common.project_utils as project
 %><%include file="common.template.sh" />
 
+CheckProcessRunning "$SERVER_PID_FILE_NAME";
+if [ 0 -eq $? ]; then
+	NoticeMsg "$SERVER_FULL_NAME already stopped";
+	exit 0;
+fi
+
 ./$SERVERD_NAME -id $SERVER_BUS_ID -c ../etc/$SERVER_FULL_NAME.conf -p $SERVER_PID_FILE_NAME stop
 
 export LD_PRELOAD=;
