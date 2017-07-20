93330504d9d3df133d445108934eb9fd80217e43
owent <owt5008137@live.com>
2017-07-20 17:08:27 +0800

    优化flatbuffer下载脚本 优先使用wget而不是curl，wget更稳点

--- 3rd_party/flatbuffers/flatbuffers.cmake
+++ 3rd_party/flatbuffers/flatbuffers.cmake
@@ -6,21 +6,26 @@ endif()
 
 set (3RD_PARTY_FLATBUFFER_REPO_DIR "${3RD_PARTY_FLATBUFFER_BASE_DIR}/repo")
 set (3RD_PARTY_FLATBUFFER_VERSION v1.7.1)
-set (3RD_PARTY_FLATBUFFER_HEADER "${3RD_PARTY_FLATBUFFER_REPO_DIR}/include/flatbuffers/flatbuffers.h")
+set (3RD_PARTY_FLATBUFFER_HEADERS
+    "include/flatbuffers/flatbuffers.h"
+    "include/flatbuffers/base.h"
+)
 
-if(NOT EXISTS ${3RD_PARTY_FLATBUFFER_REPO_DIR} OR NOT EXISTS ${3RD_PARTY_FLATBUFFER_HEADER})
-    if (NOT EXISTS "${3RD_PARTY_FLATBUFFER_REPO_DIR}/include/flatbuffers")
-        file(MAKE_DIRECTORY "${3RD_PARTY_FLATBUFFER_REPO_DIR}/include/flatbuffers")
+foreach (3RD_PARTY_FLATBUFFER_HEADER IN LISTS 3RD_PARTY_FLATBUFFER_HEADERS)
+    if(NOT EXISTS ${3RD_PARTY_FLATBUFFER_REPO_DIR} OR NOT EXISTS "${3RD_PARTY_FLATBUFFER_REPO_DIR}/${3RD_PARTY_FLATBUFFER_HEADER}")
+        if (NOT EXISTS "${3RD_PARTY_FLATBUFFER_REPO_DIR}/include/flatbuffers")
+            file(MAKE_DIRECTORY "${3RD_PARTY_FLATBUFFER_REPO_DIR}/include/flatbuffers")
+        endif()
+
+        message(STATUS "start to download flatbuffers header ${3RD_PARTY_FLATBUFFER_HEADER}")
+        FindConfigurePackageDownloadFile("https://raw.githubusercontent.com/google/flatbuffers/${3RD_PARTY_FLATBUFFER_VERSION}/${3RD_PARTY_FLATBUFFER_HEADER}" "${3RD_PARTY_FLATBUFFER_REPO_DIR}/${3RD_PARTY_FLATBUFFER_HEADER}")
     endif()
 
-    message(STATUS "start to download flatbuffers")
-    FindConfigurePackageDownloadFile("https://raw.githubusercontent.com/google/flatbuffers/${3RD_PARTY_FLATBUFFER_VERSION}/include/flatbuffers/flatbuffers.h" ${3RD_PARTY_FLATBUFFER_HEADER})
-endif()
-
-if (NOT EXISTS ${3RD_PARTY_FLATBUFFER_HEADER})
-    EchoWithColor(COLOR RED "-- Dependency: Flatbuffer is required")
-    message(FATAL_ERROR "Flatbuffer not found")
-endif()
+    if (NOT EXISTS "${3RD_PARTY_FLATBUFFER_REPO_DIR}/${3RD_PARTY_FLATBUFFER_HEADER}")
+        EchoWithColor(COLOR RED "-- Dependency: Flatbuffer ${3RD_PARTY_FLATBUFFER_HEADER} is required")
+        message(FATAL_ERROR "Flatbuffer not found")
+    endif()
+endforeach()
 
 set (3RD_PARTY_FLATBUFFER_INC_DIR "${3RD_PARTY_FLATBUFFER_REPO_DIR}/include")
 set (3RD_PARTY_FLATBUFFER_SRC_DIR "${3RD_PARTY_FLATBUFFER_REPO_DIR}/src")
