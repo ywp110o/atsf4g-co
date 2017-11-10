e40b37f3df6c1a1d0b89a7e3f4b86405b5bf9535
owent <owt5008137@live.com>
2017-11-10 14:07:24 +0800

    atgateway 介入ECDH算法及测试完成

--- atframework/service/atgateway/atgateway.cpp
+++ atframework/service/atgateway/atgateway.cpp
@@ -172,9 +172,9 @@ public:
             cfg.dump_to("atgateway.client.crypt.dhparam", crypt_conf.dh_param);
             if (!crypt_conf.dh_param.empty()) {
                 if (0 == UTIL_STRFUNC_STRNCASE_CMP("ecdh:", crypt_conf.dh_param.c_str(), 5)) {
-                    crypt_conf.switch_secret_type = ::atframe::gw::inner::v1::switch_secret_t_EN_SST_DH;
-                } else {
                     crypt_conf.switch_secret_type = ::atframe::gw::inner::v1::switch_secret_t_EN_SST_ECDH;
+                } else {
+                    crypt_conf.switch_secret_type = ::atframe::gw::inner::v1::switch_secret_t_EN_SST_DH;
                 }
             }
 
